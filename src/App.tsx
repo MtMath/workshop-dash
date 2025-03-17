6acf33595da8fc9bdead15f10fa9890f3ed552cd
Matheus Costa <matheus.design12@gmail.com>
2025-03-17 08:27:30 -0300

    chore: Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import AppRouter from "./Router";
+import { CssBaseline } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
