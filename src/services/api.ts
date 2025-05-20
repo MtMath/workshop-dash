a5c9459b62d2980095c4ce1a5ddb48c6dc9d0174
Matheus Costa <matheus.design12@gmail.com>
2025-05-20 17:35:53 -0300

    fix: force url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || "http://localhost:8080/api/v1",
+  baseURL:
+    process.env.API_URL ||
+    "https://workshopsapp-production.up.railway.app/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
